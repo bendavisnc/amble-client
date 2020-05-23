f631c1a8e419165e6e4cee7a2a9d511f125353b0
Ben Davis <ben.davis@signalpath.com>
2020-05-23 18:19:32 -0400

    Goes back to deleting a lot.

--- src/cljs/amble_client/core.cljs
+++ src/cljs/amble_client/core.cljs
@@ -1,81 +1,10 @@
 (ns amble-client.core
   (:require
    [reagent.core :as reagent :refer [atom]]
-   [reagent.dom :as rdom]
-   [reagent.session :as session]
-   [reitit.frontend :as reitit]
-   [clerk.core :as clerk]
-   [accountant.core :as accountant]))
+   [reagent.dom :as rdom]))
 
-(defn stupid-simple []
-  ["div" {:id "immaStupidId"}])
+(defn wut []
+  [:div {:class neat :id neato} "hi"])
 
-
-;; -------------------------
-;; Routes
-
-(def router
-  (reitit/router
-   [["/" :index]
-    ["/items"
-     ["" :items]
-     ["/:item-id" :item]]
-    ["/about" :about]]))
-
-(defn path-for [route & [params]]
-  (if params
-    (:path (reitit/match-by-name router route params))
-    (:path (reitit/match-by-name router route))))
-
-;; -------------------------
-;; Page components
-
-(defn home-page []
-  stupid-simple)
-
-(defn unknown []
-  "idk man")
-
-
-
-;; -------------------------
-;; Translate routes -> page components
-
-(defn page-for [route]
-  (case route
-    :index #'home-page
-    default #'unknown))
-
-
-;; -------------------------
-;; Page mounting component
-
-(defn current-page []
-  (fn []
-    (let [page (:current-page (session/get :route))]
-      [page])))
-
-;; -------------------------
-;; Initialize app
-
-(defn mount-root []
-  (rdom/render [current-page] (.getElementById js/document "app")))
-
-(defn init! []
-  (clerk/initialize!)
-  (accountant/configure-navigation!
-   {:nav-handler
-    (fn [path]
-      (let [match (reitit/match-by-path router path)
-            current-page (:name (:data  match))
-            route-params (:path-params match)]
-        (reagent/after-render clerk/after-render!)
-        (session/put! :route {:current-page (page-for current-page)
-                              :route-params route-params})
-        (clerk/navigate-page! path)
-        ))
-    :path-exists?
-    (fn [path]
-      (boolean (reitit/match-by-path router path)))})
-  (accountant/dispatch-current!)
-  (mount-root))
+(defn init![]
+  (rdom/render [wut] (.getElementById js/document "app")))
