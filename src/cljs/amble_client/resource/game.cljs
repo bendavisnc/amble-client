4e033c433c1685b362cb585b294ec2d561f608c9
Ben Davis <ben.davis@signalpath.com>
2020-05-25 16:13:14 -0400

    bypassing cors for now

--- src/cljs/amble_client/resource/game.cljs
+++ src/cljs/amble_client/resource/game.cljs
@@ -13,14 +13,43 @@
 (def url-openapi
   (str "http://" host ":" port "/openapi.json"))
 
+(def interceptor-custom {
+                         :name ::interceptor
+                         :leave (fn [req]
+                                  (.log js/console "hello hello")
+                                  (.log js/console (clj->js req))
+                                  (assoc-in req
+                                            [:request, :with-credentials?]
+                                            false))})
+
+; (def interceptors-custom (-> martian-http/default-interceptors
+                            ;  drop-last
+                            ;  (concat [interceptor-custom])
+                            ;  (concat [(last martian-http/default-interceptors)])))
+
+(def interceptors-custom (-> martian-http/default-interceptors
+                             (concat [interceptor-custom])))
+                             
+     
 (def api-promise
   (new js/Promise (fn [resolve-callback, reject-callback]
                     (casync/go
-                      (resolve-callback (casync/<! (martian-http/bootstrap-swagger url-openapi))))
+                      (resolve-callback (casync/<! (martian-http/bootstrap-swagger url-openapi {:interceptors interceptors-custom}))))
                     nil)))
 
-(defn get! [game-id]
+(defn response-promise [endpoint-key, param-map]
   (.then api-promise
     (fn [api]
-      (martian/explore api))))
-  
+      (.log js/console "this api...")
+      (.log js/console api)
+      (aset api "api_root" "http://localhost:3000")
+      (new js/Promise (fn [resolve-callback, reject-callback]
+                        (casync/go
+                          (resolve-callback (casync/<! (martian/response-for api endpoint-key param-map))))
+                        nil)))))
+
+
+(defn get! [game-id]
+  (response-promise :get-game-by-id {:game-id game-id}))
+ 
+ 
