b4ab1a49534f042b9f7dee6fcc75324281a133f2
Ben Davis <ben.davis@signalpath.com>
2020-10-09 22:32:35 -0400

    Adds wes anderson css.

--- src/clj/amble_client/handler.clj
+++ src/clj/amble_client/handler.clj
@@ -17,7 +17,9 @@
    [:meta {:charset "utf-8"}]
    [:meta {:name "viewport"
            :content "width=device-width, initial-scale=1"}]
-   (include-css (if (env :dev) "/css/site.css" "/css/site.min.css"))])
+   (include-css 
+    (if (env :dev) "/css/site.css" "/css/site.min.css"))
+   (include-css "/css/wesandersontry.css")])
 
 (defn loading-page []
   (html5
