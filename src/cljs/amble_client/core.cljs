1be83e11864bce2110c72c85a7fd720425986807
Ben Davis <ben.davis@signalpath.com>
2020-10-10 23:48:38 -0400

    neat

--- src/cljs/amble_client/core.cljs
+++ src/cljs/amble_client/core.cljs
@@ -11,37 +11,29 @@
 (defn init!*
   ([]
    (let [game-id (utils/game-id-from-window)
-         game-chan (game-resource/get! game-id)]
-     (casync/take! game-chan (fn [whatever-game]
-                               (println "really?")
-                               (println whatever-game))))))
-  ;       game-promise (game-resource/get! game-id)
-  ;       on-successful-response (fn [game-response]
-  ;                                (if (not (:success game-response))
-  ;                                  (throw (new js/Error (:error-text game-response))))
-  ;                                (let [{:keys [designatee-coords, piece-indexes]}
-  ;                                      (:body game-response)]
-  ;                                  (init!* :game-id game-id
-  ;                                          :designatee-coords designatee-coords
-  ;                                          :piece-indexes piece-indexes)))]
-  ;   (-> game-promise
-  ;       (.then on-successful-response)
-  ;       (.catch (fn [err]
-  ;                 (amble-client-state/update! :errors [err])
-  ;                 (throw err))))))
-  ;
-  ;([& {:keys [game-id, designatee-coords, piece-indexes]}]
-  ; (let [on-after-ui-init (fn [_]
-  ;                          (amble-client-interaction/init!)
-  ;                          (println (str "Finished initializing game, "
-  ;                                         game-id)))]
-  ;   (amble-client-state/init! :game-id game-id
-  ;                             :designatee-coords designatee-coords
-  ;                             :piece-indexes piece-indexes)
-  ;   (rdom/render
-  ;     [markup/app-markup-error-checked]
-  ;     (.getElementById js/document "app")
-  ;     on-after-ui-init))))
+         game-chan (game-resource/get! game-id)
+         on-successful-response (fn [game-response]
+                                  (if (not (:success game-response))
+                                    (throw (new js/Error (:error-text game-response))))
+                                  (let [{:keys [designatee-coords, piece-indexes]}
+                                        (:body game-response)]
+                                    (init!* :game-id game-id
+                                            :designatee-coords designatee-coords
+                                            :piece-indexes piece-indexes)))]
+     (casync/take! game-chan on-successful-response)))
+
+  ([& {:keys [game-id, designatee-coords, piece-indexes]}]
+   (let [on-after-ui-init (fn [_]
+                            (amble-client-interaction/init!)
+                            (println (str "Finished initializing game, "
+                                           game-id)))]
+     (amble-client-state/init! :game-id game-id
+                               :designatee-coords designatee-coords
+                               :piece-indexes piece-indexes)
+     (rdom/render
+       [markup/app-markup-error-checked]
+       (.getElementById js/document "app")
+       on-after-ui-init))))
 
 (defn init! []
   (println "Starting client init.")
