c358b64a5f047e9f1829989a6520243fafb8b71e
ben <bendavisnc@gmail.com>
2022-08-07 20:26:55 -0400

    Meh, a little janky.

--- src/cljs/amble_client/interpolate_function.cljs
+++ src/cljs/amble_client/interpolate_function.cljs
@@ -2,38 +2,48 @@
   (:require [cljs.core.async :as async])
   (:require-macros [cljs.core.async :refer [go, go-loop]]))
 
-;; Animations last a second, by default.
-(def time-duration 1000)
+(def time-duration 250)
 
 ;; How many times the animation function provided is called
-(def interpolation-discreet-count 60)
+(def interpolation-discreet-count 24)
 
 (def f-chan (async/chan))
 (def line-points-chan (async/chan))
 
 (defn loop-animation [f, x1, y1, x2, y2]
-  (loop [index 0]
-    (if (< interpolation-discreet-count 
-           index)
-      (println (str "Finished animation, time," (new js/Date) "."))
-      (let [i (/ index 
-                 (* 1.0 
-                    interpolation-discreet-count))
-            xi (+ x1
-                  (* i
-                     (- x2 x1)))
-            yi (+ y1
-                  (* i
-                     (- y2 y1)))]
-        (println [i, xi, yi])
-        (f xi yi)
-        (recur (inc index))))))
+  (let [wait-time-amount 
+        (.floor js/Math
+                (/ (* 1.0 time-duration)  
+                   interpolation-discreet-count))]
+        ;; _ (println wait-time-amount)]
+    (letfn [
+            (recursive-call [index]
+              (if (< interpolation-discreet-count 
+                     index)
+                (println (str "Finished animation, time," (new js/Date) "."))
+                (let [i (/ index 
+                           (* 1.0 
+                              interpolation-discreet-count))
+                      xi (+ x1
+                            (* i
+                               (- x2 x1)))
+                      yi (+ y1
+                            (* i
+                               (- y2 y1)))]
+                  (f {:x xi
+                      :y yi})
+                  (js/setTimeout (fn []
+                                   (recursive-call (inc index)))
+                                 wait-time-amount))))]
+                                 
+      (recursive-call 0))))
 
 (go (loop []
       (let [f (async/<! f-chan)
             line-points (async/<! line-points-chan)
             [x1, y1, x2, y2] line-points]
         (loop-animation f, x1, y1, x2, y2))
+        ;; (f {:x x2 :y y2}));}))
       (recur)))
         
 
--- src/cljs/amble_client/move_remote.cljs
+++ src/cljs/amble_client/move_remote.cljs
@@ -3,6 +3,7 @@
   (:require [integrant.core :as ig]
             [cljs.core.async :as async]
             [amble-client.resource.environment :refer [environment]]
+            [amble-client.interpolate-function :refer [interpolate-function]]
             [haslett.client :as haslett-client])
   (:require-macros [cljs.core.async :refer [go, go-loop]]))
 
@@ -11,11 +12,19 @@
 (def move-chan (async/chan))
 
 (go-loop [app-atom (async/<! app-atom-chan)]
-  (let [{:keys [player-id, player-piece-index, x, y] :as move} (async/<! move-chan)]
-    (println "Handling remote move. - todo")
+  (let [{:keys [player-id, player-piece-index, x, y, move]} (async/<! move-chan)
+        [last-x, last-y] (last move)]
+    (println "Handling remote move.")
+    (interpolate-function (fn [{:keys [x, y]}]
+                            (swap! app-atom assoc-in [:player-pieces (keyword player-id) (js/parseInt player-piece-index)] [x, y]))
+                          :x1 last-x
+                          :y1 last-y
+                          :x2 x
+                          :y2 y)
+                     
     ;; (println move)
-    (swap! app-atom assoc-in [:player-pieces (keyword player-id) (js/parseInt player-piece-index)] [x, y])
     (recur app-atom)))
+
 
 (defmethod ig/init-key :amble/move-remote [_ {:keys [app-atom, move-remote-chan]}]
   (async/pipe move-remote-chan amble-client.move-remote/move-chan)
