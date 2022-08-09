21f2806ce8b90b3db5f6ec806287ee305b96ca25
ben <bendavisnc@gmail.com>
2022-08-09 11:19:19 -0400

    Tweaks piece active style.

--- src/cljs/amble_client/board_piece_active.cljs
+++ src/cljs/amble_client/board_piece_active.cljs
@@ -8,17 +8,31 @@
 (def move-xy-chan (async/chan))
 (def board-piece-closest-chan (async/chan))
 
+
+
+(defn set-active! [app-atom, board-piece, is-active?]
+  (swap! app-atom assoc-in [:board-pieces (:index board-piece)] (assoc board-piece :is-active? is-active?)))
+
+(defn activate! [app-atom, board-piece]
+  (set-active! app-atom board-piece true)) 
+
+(defn deactivate! [app-atom, board-piece]
+  (set-active! app-atom board-piece false)) 
+
 (go-loop [app-atom (async/<! app-atom-chan)
           board-piece-closest (async/<! board-piece-closest-chan)]
-  (let [_ (println "hiiiiiiii")
+  (let [
         {:keys [x,y]} (async/<! move-xy-chan)
         board-piece (board-piece-closest x, y)]
-    (println "Updating active board piece.")
-    (swap! app-atom assoc-in [:board-pieces (:index board-piece)] (assoc board-piece :is-active? true))
+    (when-let [board-piece-active-last
+               (first (filter :is-active?
+                              (:board-pieces (deref app-atom))))]
+      (deactivate! app-atom board-piece-active-last))
+
+    (activate! app-atom board-piece)
     (recur app-atom, board-piece-closest)))   
 
 (defmethod ig/init-key :amble/board-piece-active [_ {:keys [app-atom, board-piece-closest, move-xy-chan, app-ready-chan]}]
-  (println "whhhhhhhhhhhhhht")
   (async/pipe move-xy-chan amble-client.board-piece-active/move-xy-chan)
   (go
     (async/<! app-ready-chan)
