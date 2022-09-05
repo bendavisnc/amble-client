c0dd8b017c778ff51bbbe03946f0a429185e13e5
ben <bendavisnc@gmail.com>
2022-09-05 16:39:16 -0400

    Trying out shadow cljs.
    `npx create-cljs-project amble-client`

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java $JVM_OPTS -cp target/amble-client.jar clojure.main -m amble-client.server
--- env/prod/cljs/amble_client/prod.cljs
+++
@@ -1,7 +0,0 @@
-(ns amble-client.prod
-  (:require [amble-client.core :as core]))
-
-;;ignore println statements in prod
-(set! *print-fn* (fn [& _]))
-
-(core/init!)
