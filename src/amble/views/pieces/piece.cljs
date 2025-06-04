57f7bcc2fd994f8bc307888d95524d23bc334874
bendavisnc <bendavisnc@gmail.com>
2025-06-04 12:48:35 -0400

    Adds player ui events.

--- src/amble/views/pieces/piece.cljs
+++ src/amble/views/pieces/piece.cljs
@@ -2,10 +2,11 @@
 
 (def piece-size 0.023)
 
-(defn piece [& {:keys [x, y, size, id, class]}]
-  [:circle {:cx x
-            :cy y
-            :r size
-            :key id
-            :id id
-            :class class}])
+(defn piece [& {:keys [x, y, size, id, class, extra-opts]}]
+  [:circle (merge {:cx x
+                   :cy y
+                   :r size
+                   :key id
+                   :id id
+                   :class class}
+                  extra-opts)])
--- src/amble/views/pieces/player.cljs
+++ src/amble/views/pieces/player.cljs
@@ -1,9 +1,36 @@
 (ns amble.views.pieces.player
   (:require
    [amble.views.pieces.piece :as piece]
-   [goog.string :as gstring]))
+   [goog.string :as gstring]
+   [re-frame.core :as re-frame]))
 
 (def classname "player-piece")
+
+(defn userfeedback-handler [event]
+  (cond (= "mousedown"
+           (.-type event))
+        (re-frame/dispatch [::move-start event])
+        (= "mousemove"
+           (.-type event))
+        (re-frame/dispatch [::move-update event])
+        (= "mouseup"
+           (.-type event))
+        (re-frame/dispatch [::move-end event])
+        (= "mousemove"
+           (.-type event))
+        (re-frame/dispatch [::move-update event])
+        (= "touchstart"
+           (.-type event))
+        (re-frame/dispatch [::move-start event])
+        (= "touchmove"
+           (.-type event))
+        (re-frame/dispatch [::move-update event])
+        (= "touchend"
+           (.-type event))
+        (re-frame/dispatch [::move-end event])
+        :else
+          (throw (new js/Error
+                      (gstring/format "No user feedback handler defined for %s" (.-type event)))))) 
 
 (defn pieces [players]
   [:<>
@@ -16,6 +43,11 @@
                      :y y
                      :size piece/piece-size
                      :id (gstring/format "%s-%s-%s" classname (name player-id), index)
-                     :class [classname, "player", (name player-id)]))])])
+                     :class [classname, "player", (name player-id)]
+                     :extra-opts {:on-mouse-down userfeedback-handler
+                                  :on-mouse-up userfeedback-handler
+                                  :on-touch-start userfeedback-handler
+                                  :on-touch-end userfeedback-handler
+                                  :on-touch-move userfeedback-handler}))])])
 
 (comment (gstring/format "player piece %s"))
