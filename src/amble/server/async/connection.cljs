657f121657f31aaa101153cd1bede57363ddbf77
bendavisnc <bendavisnc@gmail.com>
2025-06-24 10:02:43 -0400

    Separates out websockets connection.

---
+++ src/amble/server/async/connection.cljs
@@ -0,0 +1,24 @@
+(ns amble.server.async.connection
+  (:require
+   [amble.config :as amble-config]
+   [amble.server.async.server :as async-server]
+   [goog.string :as gstring]
+   [goog.string.format]
+   [re-frame.core :as re-frame]))
+
+(defonce socket-atom (atom nil))
+
+(defn websockets-url [game-id]
+  (gstring/format "ws://%s:%d/move/async/?game-id=%s"
+                  amble-config/SERVER_HOST
+                  amble-config/SERVER_PORT
+                  (name game-id)))
+
+(defn connect-websocket! [game-id]
+  (let [url (websockets-url game-id)
+        ws (js/WebSocket. url)]
+    (set! (.-onmessage ws) #(re-frame/dispatch [::async-server/on-message (.-data %)]))
+    (set! (.-onopen ws) #(re-frame/dispatch [::async-server/on-open]))
+    (set! (.-onclose ws) #(re-frame/dispatch [::async-server/on-close]))
+    (set! (.-onerror ws) #(re-frame/dispatch [::async-server/on-error %]))
+    (reset! socket-atom ws)))
--- src/amble/server/async/main.cljs
+++ src/amble/server/async/main.cljs
@@ -1,40 +1,24 @@
 (ns amble.server.async.main
   (:require
-   [amble.config :as amble-config]
    [amble.models.pieces.player :as player-model]
+   [amble.server.async.connection :as connection]
    [amble.server.async.server :as async-server]
-   [goog.string :as gstring]
    [goog.string.format]
    [re-frame.core :as re-frame]))
-
-(defonce socket-atom (atom nil))
-
-(defn websockets-url [game-id]
-  (gstring/format "ws://%s:%d/move/async/?game-id=%s"
-                  amble-config/SERVER_HOST
-                  amble-config/SERVER_PORT
-                  (name game-id)))
-
-(defn connect-websocket! [url]
-  (let [ws (js/WebSocket. url)]
-    (.dir js/console ws)
-    (set! (.-onmessage ws) #(re-frame/dispatch [::async-server/on-message (.-data %)]))
-    (set! (.-onopen ws) #(re-frame/dispatch [::async-server/on-open]))
-    (set! (.-onclose ws) #(re-frame/dispatch [::async-server/on-close]))
-    (set! (.-onerror ws) #(re-frame/dispatch [::async-server/on-error %]))
-    (reset! socket-atom ws)))
-
-(re-frame/reg-fx
-  ::initialize
-  (fn [game-id]
-    (let [url (websockets-url game-id)]
-      (connect-websocket! url))))
 
 (re-frame/reg-event-fx
   ::async-server/initialize
   (fn [{:keys [db]} [_ game-id]]
+    ;; Use the connection module to create a websocket connection.
+    (connection/connect-websocket! game-id)
+    {:db (assoc db :game-id game-id)}))
+
+(re-frame/reg-event-fx
+  ::async-server/on-message
+  (fn [{:keys [db]} [_ message]]
     {:db db
-     ::initialize game-id}))
+     :dispatch [::player-model/on-move-remote message]}))
+
 
 (re-frame/reg-event-fx
   ::async-server/on-open
@@ -42,13 +26,8 @@
 
 (re-frame/reg-event-fx
   ::async-server/on-close
-  (fn [_ _]))
-
-(re-frame/reg-event-fx
-  ::async-server/on-message
-  (fn [{:keys [db]} [_ message]]
-    {:db db
-     :dispatch [::player-model/on-move-remote message]}))
+  (fn [{:keys [db]} _]
+    {:db db}))
 
 (re-frame/reg-event-fx
   ::async-server/on-error
