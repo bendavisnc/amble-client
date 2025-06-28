d1d7b1112153b493585d2895aa753e23ff4e26e6
bendavisnc <bendavisnc@gmail.com>
2025-06-28 17:35:10 -0400

    Adds minor changes.

--- src/amble/dev.cljs
+++ src/amble/dev.cljs
@@ -6,56 +6,64 @@
    [amble.server.server :as server]
    [re-frame.core :as re-frame]))
 
-(defn printdb
-  "Prints the current db to the console."
+(defn print-db
+  "Dispatches an event to print the current :game portion of the app-db."
   []
-  (re-frame/dispatch [::printdb]))
+  (re-frame/dispatch [::print-db]))
 
 (re-frame/reg-event-db
-  ::printdb
-  (fn [db, _]
-    (println (:game db))))
+  ::print-db
+  (fn [db _]
+    (println (:game db))
+    db))
 
-(defn index-to-player [i]
+(defn index-to-player
+  "Maps an integer index to a player keyword."
+  [i]
   (case i
     0 :player-one
     1 :player-two
     2 :player-three
     3 :player-four
     4 :player-five
-    5 :player-six))
+    5 :player-six
+    nil))
 
-(defn setplayer [i]
-  (re-frame/dispatch [::setplayer i]))
+(defn set-player
+  "Dispatches an event to set the current player in the db."
+  [i]
+  (re-frame/dispatch [::set-player i]))
 
 (re-frame/reg-event-db
-  ::setplayer
-  (fn [db, [_ i]]
-    (assoc-in db
-              [:game :settings :player]
+  ::set-player
+  (fn [db [_ i]]
+    (assoc-in db [:game :settings :player]
               (index-to-player i))))
 
 (re-frame/reg-event-fx
   ::on-game-delete-failure
-  (fn [coeff, [_ event]]
-    (println "Failed to delete game: " event)))
+  (fn [_ [_ event]]
+    (println "Failed to delete game:" event)
+    {}))
 
 (re-frame/reg-event-fx
   ::on-game-delete-success
-  (fn [_, [_ event]]
-    (println "Game deleted successfully: " event)))
+  (fn [_ [_ event]]
+    (println "Game deleted successfully:" event)
+    {}))
 
 (re-frame/reg-event-fx
-  ::deletegame
-  (fn [cofx [_ _]]
-    (let [game-id (db-to-game-id (:db cofx))]
-      {:dispatch [::server/delete-game game-id ::on-game-delete-success, ::on-game-delete-failure]})))
+  ::delete-game
+  (fn [{:keys [db]} _]
+    (let [game-id (db-to-game-id db)]
+      {:dispatch [::server/delete-game game-id ::on-game-delete-success ::on-game-delete-failure]})))
 
-(defn deletegame [i]
-  (re-frame/dispatch [::deletegame]))
+(defn delete-game []
+  (re-frame/dispatch [::delete-game]))
 
-(defn throwerror
+(defn throw-error
   "Throws an error to test error handling."
   []
   (let [test-error (new js/Error "Hi, I'm a test error in this world.")]
     (re-frame/dispatch [::errors/error "" test-error])))
+
