6fc8a8aea10442331a479144d36a5d65858c771b
bendavisnc <bendavisnc@gmail.com>
2025-06-28 21:16:02 -0400

    Updates parsing of hash params.

--- src/amble/models/hash_params.cljs
+++ src/amble/models/hash_params.cljs
@@ -1,16 +1,16 @@
 (ns amble.models.hash-params
   "Provides a coeffects injector interceptor for hash params found from the address bar url."
   (:require
-   [clojure.string :as string]
+   [goog.string :as gstring]
    [re-frame.core :as re-frame]))
 
-(defn- parse-hash-params [hash]
-  (let [cleaned (subs hash 1)
-        pairs (string/split cleaned #"&")]
+(defn parse-hash-params [query]
+  (let [hash (.substring query 1) ; remove the leading '#'
+        params (js/URLSearchParams. hash)]
     (into {}
-          (map #(let [[k v] (string/split % #"=")]
-                  [(keyword k) v])
-               pairs))))
+          (for [key (js->clj (.keys params))]
+            [(keyword key)
+             (.get params key)]))))
 
 ;; Adds a key for hash param values from the address bar
 ;; Based on whatever follows the `#` in the URL.
@@ -28,6 +28,8 @@
   ::update
   [interceptor]
   (fn [{:keys [db, hash-params]} [_]]
+    (println (gstring/format "Applying hash params, `%s`."
+                             hash-params))
     {:db
      (if-let [player-id-from-addressbar (some-> hash-params :player keyword)]
        (assoc-in db [:game :settings :player] player-id-from-addressbar)
