e5a256d943749fdd841994b8dd7be6a202b01743
bendavisnc <bendavisnc@gmail.com>
2025-08-01 21:06:38 -0400

    Blessed colon.

--- src/amble/server/async/connection.cljs
+++ src/amble/server/async/connection.cljs
@@ -11,7 +11,7 @@
 (defn websockets-url [game-id]
   (when (= "localhost" amble-config/SERVER_HOST)
     (println "Using `localhost` for websocket url."))
-  (let [ws-protocol (if (= "https" 
+  (let [ws-protocol (if (= "https:" 
                            (.-protocol (.-location js/window)))
                        "wss"
                        "ws")]
